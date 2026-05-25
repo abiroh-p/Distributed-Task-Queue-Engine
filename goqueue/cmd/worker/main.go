60bf9cb0c3d815d612d545e96a1d208886b5e9a2
Abishek <abishek.123.prasad@gmail.com>
2026-05-25 11:16:15 +0530

    feat: worker entrypoint with hostname-based worker ID

--- goqueue/cmd/worker/main.go
+++ goqueue/cmd/worker/main.go
@@ -1,18 +1,46 @@
 package main
 
 import (
-	"context"
-	"log"
-	"os/signal"
-	"syscall"
+    "context"
+    "os"
+    "os/signal"
+    "syscall"
+
+    "github.com/google/uuid"
+    "github.com/rs/zerolog/log"
+
+    "github.com/abishekP101/goqueue/config"
+    "github.com/abishekP101/goqueue/internal/broker"
+    "github.com/abishekP101/goqueue/internal/store"
+    "github.com/abishekP101/goqueue/internal/worker"
 )
 
-func main() {
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
-	defer stop()
+func workerID() string {
+    if host, err := os.Hostname(); err == nil && host != "" {
+        return host
+    }
+    return uuid.New().String()
+}
 
-	log.Println("goqueue worker node starting...")
-	// TODO: wire up worker.Pool, broker client
-	<-ctx.Done()
-	log.Println("worker draining in-flight jobs...")
+func main() {
+    ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+    defer stop()
+
+    cfg := config.Load()
+
+    s, err := store.New(cfg.PostgresDSN)
+    if err != nil {
+        log.Fatal().Err(err).Msg("failed to connect to postgres")
+    }
+    log.Info().Msg("postgres connected")
+
+    b := broker.New(cfg.RedisAddr)
+    log.Info().Msg("broker connected")
+
+    id := workerID()
+    pool := worker.New(b, s, id, int64(cfg.WorkerCount))
+
+    log.Info().Str("worker_id", id).Int("concurrency", cfg.WorkerCount).Msg("worker starting")
+
+    pool.Run(ctx)
 }
