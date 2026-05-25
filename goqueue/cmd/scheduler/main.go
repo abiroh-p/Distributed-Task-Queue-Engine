f59d7903fd935907bd33706586c1f8499b01ce55
Abishek <abishek.123.prasad@gmail.com>
2026-05-25 11:35:21 +0530

    feat: scheduler entrypoint

--- goqueue/cmd/scheduler/main.go
+++ goqueue/cmd/scheduler/main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
-	"context"
-	"log"
-	"os/signal"
-	"syscall"
+    "context"
+    "os/signal"
+    "syscall"
+
+    "github.com/rs/zerolog/log"
+
+    "github.com/abishekP101/goqueue/config"
+    "github.com/abishekP101/goqueue/internal/broker"
+    "github.com/abishekP101/goqueue/internal/scheduler"
+    "github.com/abishekP101/goqueue/internal/store"
 )
 
 func main() {
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
-	defer stop()
+    ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+    defer stop()
 
-	log.Println("goqueue scheduler starting...")
-	// TODO: wire up scheduler.Engine, store
-	<-ctx.Done()
-	log.Println("scheduler stopped.")
+    cfg := config.Load()
+
+    s, err := store.New(cfg.PostgresDSN)
+    if err != nil {
+        log.Fatal().Err(err).Msg("failed to connect to postgres")
+    }
+    log.Info().Msg("postgres connected")
+
+    b := broker.New(cfg.RedisAddr)
+    log.Info().Msg("broker connected")
+
+    engine := scheduler.New(b, s)
+
+    log.Info().Msg("scheduler starting")
+    engine.Run(ctx)
 }
